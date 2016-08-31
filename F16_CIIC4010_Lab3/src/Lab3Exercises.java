efd8e99958b10526f96f32b0d41a3d7d66f1fc4e
Andy <Andy@DESKTOP-P5H1KC8>
2016-08-30 23:32:25 -0400

    The name of the frame was changed.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Arnaldo Colon");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(1368, 912);
+		myFrame.setSize(400,400 );
 		myFrame.setVisible(true);
 	}
 }
