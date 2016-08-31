20d038c1dd1cae9bfb9724fe91c097d322363440
4015c3 <4015c3@china.ece.uprm.edu>
2016-08-31 12:11:56 -0400

    Rectangle within rectangle

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,8 +4,10 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Arnaldo  Colon");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(1368, 912);
-		myFrame.setSize(400,400 );
+		myFrame.setLocation(1024,576);
+		myFrame.setSize(200,200 );
+		 MyPanelClass myPanel = new MyPanelClass();
+         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
