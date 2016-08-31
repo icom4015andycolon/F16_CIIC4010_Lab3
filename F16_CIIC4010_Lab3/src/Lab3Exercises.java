dc7335d9497d07a915eb6408420430cc03e2dbc1
Andy <Andy@DESKTOP-P5H1KC8>
2016-08-30 23:50:55 -0400

    Changes title of frame.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Arnaldo Colon");
+		JFrame myFrame = new JFrame("Arnaldo  Colon");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(1368, 912);
 		myFrame.setSize(400,400 );
